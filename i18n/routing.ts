7af5ca780d5a25345616fa4a1760bd5b93cfed08
garfield2630 <xiaomaowa37@gmail.com>
2025-12-14 20:19:58 +0800

    修改默认语言

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,5 +2,5 @@ export const locales = ["zh", "en"] as const;
 
 export type Locale = (typeof locales)[number];
 
-export const defaultLocale: Locale = "zh";
+export const defaultLocale: Locale = "en";
 
